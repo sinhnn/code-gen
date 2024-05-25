05e5a63bd9cbbb6ca3d9220d034efc80ac86122b
sinhnn <sinhnn.92@gmail.com>
2024-05-25 12:11:55 +0700

    add nested line/wrap

--- bin/sample.js
+++ bin/sample.js
@@ -7,20 +7,32 @@ function __test_js__() {
         'in': 'sample_object.json',
     };
 
-    const classSpec = new ClassSpec('Post', [
-        new PropertyInfo('id', 'integer'),
-        new PropertyInfo('url', 'string'),
-        new PropertyInfo('title', 'string'),
-        new PropertyInfo('categories', 'string'),
-        new PropertyInfo('tags', 'string'),
-        new PropertyInfo('modified_at', 'integer'),
-        new PropertyInfo('brief', 'string'),
-        new PropertyInfo('raw', 'string'),
-        new PropertyInfo('content', 'string'),
-    ]);
-
+    const specs  = [
+        new ClassSpec('Post', [
+            new PropertyInfo('id', 'integer'),
+            new PropertyInfo('url', 'string'),
+            new PropertyInfo('title', 'string'),
+            new PropertyInfo('categories', 'string'),
+            new PropertyInfo('tags', 'string'),
+            new PropertyInfo('modified_at', 'integer'),
+            new PropertyInfo('brief', 'string'),
+            new PropertyInfo('raw', 'string'),
+            new PropertyInfo('content', 'string'),
+        ]),
+        new ClassSpec('NormalizedPost', [
+            new PropertyInfo('url', 'string'),
+            new PropertyInfo('content', 'string'),
+            new PropertyInfo('title', 'string'),
+            new PropertyInfo('categories', 'string'),
+            new PropertyInfo('tags', 'string'),
+            new PropertyInfo('meta', 'object')
+        ]),
+    ];
     const js = new Javascript();
-    console.log(js.to__class(classSpec).join("\n"));
+    for(const spec of specs) {
+        console.log(js.to__class(spec).join("\n"));
+    }
+
 }
 
 __test_js__();
