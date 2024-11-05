ef06a0ae88d5d397ef62698846071e6424629455
sinhnn <sinhnn.92@gmail.com>
2024-11-05 21:02:17 +0700

    draft v0.02

--- tests/custom.js
+++ tests/custom.js
@@ -3,14 +3,24 @@ const {ClassSpec, PropertyInfo, Document} = require("../lib/spec");
 const fs = require('fs');
 const data = require("./data");
 
+
+class CustomPHP extends PHP {
+    to__getter_name(property) {
+        return `${property.name}`;
+    }
+
+    to__setter_name(property) {
+        return `set_${property.name}`;
+    }
+}
+
 (function __test__() {
 
     const doc = new Document();
     doc.addChild("<?php");
-    const lang = new PHP();
+    const lang = new CustomPHP();
     for(const spec of data.specs) {
         doc.addChild(lang.to__class(spec));
     }
-    fs.writeFileSync('results/results.php', doc.toString());
+    fs.writeFileSync('results/results.custom.php', doc.toString());
 }) ();
-
